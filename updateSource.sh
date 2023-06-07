25b22bb269fb9803cb889167d33c63d66ab77844
Michael Neises <neisesmichael@gmail.com>
2023-06-07 12:52:39 -0500

    new scripts

---
+++ updateSource.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define source and destination directories
+SOURCE_DIR="attarch/"
+DEST_DIR="test_bench/attarch/"
+
+# Define directories or files to exclude
+EXCLUDE=(
+"./am-cakeml" 
+"./linux-stable" 
+"./components/Measurement/configurations/linux_definitions.h" 
+"./components/Measurement/IntrospectionLibrary/IntrospectionLibrary.c"
+)
+
+# Build the exclude string
+EXCLUDES=''
+for exclude in "${EXCLUDE[@]}"; do
+  EXCLUDES="$EXCLUDES --exclude $exclude"
+done
+
+# Run rsync command
+eval "rsync -av --delete $EXCLUDES $SOURCE_DIR $DEST_DIR"
+
