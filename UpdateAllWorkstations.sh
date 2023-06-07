9c353f4e7332a4a70ed5e6e69944369a2ebd846f
Michael Neises <neisesmichael@gmail.com>
2023-06-06 19:50:31 -0500

    add initial update scripts, no downloads or builds

---
+++ UpdateAllWorkstations.sh
@@ -0,0 +1,10 @@
+./UpdateThisWorkstation.sh 4.9.y &
+./UpdateThisWorkstation.sh 4.19.y &
+./UpdateThisWorkstation.sh 5.4.y &
+./UpdateThisWorkstation.sh 5.10.y &
+./UpdateThisWorkstation.sh 5.15.y &
+./UpdateThisWorkstation.sh 6.1.y &
+
+wait
+
+echo "All Dissertation Workstations Updated"
